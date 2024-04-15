ed3f1cf2fdb1e55309ac7c2211c16961e26832b0
karambir <karambir@rapipay.com>
2024-04-15 12:50:44 +0530

    Initial Commit
    Project created.

---
+++ UnsplashImageApiPoc/UnsplashImageApiPoc/UnsplashImageApiPocApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnsplashImageApiPocApp.swift
+//  UnsplashImageApiPoc
+//
+//  Created by Karambir on 15/04/24.
+//
+
+import SwiftUI
+
+@main
+struct UnsplashImageApiPocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
